a0989a2a33cdc71131536efdb59d7946f5053a91
Firas <42959543+fnotorious@users.noreply.github.com>
2022-05-25 21:27:35 -0400

    Fourth commit

--- APIcall.py
+++ APIcall.py
@@ -4,8 +4,8 @@ import requests
 
 response = requests.get("http://www2.miami-dadeclerk.com/Developers/api/OfficialRecords/xml?parameter1=1992&parameter2=R438823&authKey=E28FA24F-3EEA-4D11-8630-22D6FE606328")
 
-data = response.json()
-print(json.dumps(data, indent=4))
+data = response.json()                         #<- comment this line out to try XML format
+print(json.dumps(data, indent=4))              #<- comment this line out to try XML format
 
 #string_xml = response.content                  <- uncomment this line of code if you want to attempt XML
 #tree = ElementTree.fromstring(string_xml)      <- uncomment this line of code if you want to attempt XML
